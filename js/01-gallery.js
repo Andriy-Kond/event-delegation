3df8c77ead9dff54eb161d146b58bcaf0e435af4
Andrii <andrii.kondratovich@gmail.com>
2024-04-10 20:02:43 +0300

    add: gallery markup

--- js/01-gallery.js
+++ js/01-gallery.js
@@ -1,4 +1,41 @@
-import { galleryItems } from './gallery-items.js';
-// Change code below this line
+import { galleryItems } from "./gallery-items.js";
+// import * as basicLightbox from "basiclightbox";
 
-console.log(galleryItems);
+// Change code below this line
+const gallery = document.querySelector(".gallery");
+
+const galleryItemsMarkup = galleryItems
+  .map(
+    item => `<li class="gallery__item">
+  <a class="gallery__link" href="${item.original}">
+  <img class="gallery__image" src="${item.preview}" data-source="${item.original}" alt="${item.description}" />
+  </a>
+  </li>`,
+  )
+  .join("");
+
+gallery.innerHTML = galleryItemsMarkup;
+const link = document.querySelector(".gallery__link");
+
+gallery.addEventListener("click", showOriginalSizeImg);
+link.addEventListener("click", e => {
+  e.preventDefault;
+  console.log(e.currentTarget);
+});
+
+function showOriginalSizeImg(e) {
+  e.preventDefault();
+  // console.log(e.target.dataset.source);
+
+  const instance = basicLightbox.create(`<img src="${e.target.dataset.source}" width="800" height="600">`);
+
+  instance.show();
+}
+
+window.addEventListener("keydown", closeByEsc);
+
+function closeByEsc(e) {
+  if (e.code === "Escape") {
+    closeModal();
+  }
+}
